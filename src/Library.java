de44bdd6db1a4efc84b168cccecef7685121b691
Abhisek Bagdas <abhisekbagdas@gmail.com>
2025-07-11 12:38:37 +0545

    inserted books

--- src/Library.java
+++ src/Library.java
@@ -9,7 +9,27 @@ public class Library {
     public static void main(String[] args) {
         // TODO create some books and users
         Scanner input = new Scanner(System.in);
-        Book book1 = new Book(12, "Atomic Habits", 10, "James Clear");
+        // create some books and users
+        Book book1 = new Book(12, "Atomic Habits",10, "James Clear");
+        try {
+            Connection conn = DatabaseConnection.connect();
+            String query = "INSERT INTO book" +
+                    "(bookNumber, bookName, bookAuthor, bookquantity) " +
+                    "VALUES (?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1,book1.getBookNumber());
+            ps.setString(2, book1.getBookName());
+            ps.setString(3, book1.getBookAuthor());
+            ps.setInt(4,book1.getBookQuantity());
+            if(ps.executeUpdate() > 0){
+                System.out.println("book added to database");
+            }else{
+                System.out.println("Failed to add");
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
 
         User user1 = new User("Sunil", "sunil-57", 5566);
         //show available options: available books, borrow book, return book, exit
@@ -20,63 +40,29 @@ public class Library {
         System.out.println("Enter 4: Exit");
         System.out.println("Choose an option: ");
         int option = input.nextInt();
-//        if (option == 1) {
-//            System.out.println("working on viewing available books");
-            try {
-                Connection conn = DatabaseConnection.connect();
-//                ArrayList.<Book> bookList = new ArrayList<Book>();
-                String query = "INSERT INTO book" +
-                        "(bookNumber, bookName, bookAuthor, bookQuantity) " +
-                        "VALUES (?,?,?,?)";
-                PreparedStatement ps = conn.prepareStatement(query);
-                ps.setInt(1,book1.getBookNumber());
-                ps.setString(2, book1.getBookName());
-                ps.setString(3, book1.getBookAuthor());
-                ps.setInt(4,book1.getBookQuantity());
-                if(ps.executeUpdate() > 0){
-                    System.out.println("book added to database");
-                }else{
-                    System.out.println("Failed to add");
-                }
-
-//                PreparedStatement ps = conn.prepareStatement(query);
-//
-//                ResultSet bookSet = ps.executeQuery();
-//                while (bookSet.next()) {
-//                    int bookNumber = bookSet.getInt("bookNumber");
-//                    String boonName = bookSet.getString("bookName");
-//                    int bookQuantity = bookSet.getInt("bookQuantity");
-//                    String bookAuthor = bookSet.getString("bookAuthor");
-//                    Book book = new Book(
-//                            bookSet.getInt("bookNumber"),
-//                            bookSet.getString("bookName"),
-//                            bookSet.getInt("bookQuantity"),
-//                            bookSet.getString("bookAuthor"));
-//                    boolList.add(book);
-//
-//                }
-//                for (Book book: bookList){
-//                    System.out.println("Book Number: "+book.getBookNumber());
-//                }
-
-            } catch (SQLException | ClassNotFoundException e) {
-                throw new RuntimeException(e);
-//            }else if (option == 2) {
-//            System.out.println("working on borrowing books");
+        if(option == 1){
+            System.out.println("working on viewing available books");
+        }else if(option == 2)
+        {
+            System.out.println("working on borrowing books");
             //TODO kun book borrow garne ho tyo chahi dinu parxha
             //TODO borrow garera sakesi message dekhauna parxa
-//            System.out.println("Before: " + book1.getBookQuantity());
-//            user1.borrowBook(book1, user1);
-//            System.out.println("After: " + book1.getBookQuantity());
-//        } else if (option == 3) {
-//            System.out.println("working on returning books");
-//        } else if (option == 4) {
-//            System.out.println("working on exiting");
-//        } else {
-//            System.out.println("invalid option");
-//        }
-
-        // TODO need ot figure out where to keep the Books?
+            System.out.println("Before: "+ book1.getBookQuantity());
+            user1.borrowBook(book1, user1);
+            System.out.println("After: " + book1.getBookQuantity());
+        }
+        else if(option == 3)
+        {
+            System.out.println("working on returning books");
+        }
+        else if(option == 4)
+        {
+            System.out.println("working on exiting");
+        }
+        else
+        {
+            System.out.println("invalid option");
+        }
+        //TODO need to figure out where to keep the books and users?
     }
-}
 }
